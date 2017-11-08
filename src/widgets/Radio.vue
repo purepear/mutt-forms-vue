3b636f6dc29794a52af18e14dfc5357976c2e1e3
shaundobson <shaun@boughtbymany.com>
2017-11-08 10:40:42 +0000

    Change click to change for greater browser compatibility

--- src/widgets/Radio.vue
+++ src/widgets/Radio.vue
@@ -12,7 +12,7 @@
                 v-bind:id="`${field.id}-${choice[0]}`"
                 v-bind:value="choice[0]"
                 v-bind:class="getFieldClass()"
-                v-on:click="callback(choice[0], choice[1])">
+                v-on:change="callback(choice[0], choice[1])">
             <label
                 v-bind:for="`${field.id}-${choice[0]}`"
                 class="mutt-label">{{ choice[1] }}</label>
@@ -53,14 +53,6 @@ export default {
         this.field.widget = this
     },
     data: DataProxy,
-    computed: {
-        getLabelClass() {
-            return {
-                'mutt-label': true,
-                'mutt-field-checkbox-checked': this.value
-            }
-        }
-    },
     methods: Object.assign({}, WidgetProxy, {
         getFieldClass() {
             return 'mutt-field mutt-field-radio'
