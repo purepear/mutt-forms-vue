062a8a825eaa9d2fbbca84add537506938a657c8
Delyan Haralanov <purepear@gmail.com>
2020-01-21 18:36:02 +0200

    fix: i18n warning when translation is not available

--- src/lib/i18n.js
+++ src/lib/i18n.js
@@ -1,9 +1,13 @@
 /**
  * Utility to translate strings, no-op if i18n not
  * available
+ * @param {object} instance - Vue component instance
+ * @param {string} str - String to translate
+ *
+ * @return {string}
  */
 export function i18n(instance, str) {
-  if (instance.$t) {
+  if (instance.$t && instance.$te && instance.$te(str)) {
     return instance.$t(str)
   }
   return str
