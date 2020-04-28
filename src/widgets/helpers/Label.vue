94739b08b559e4b452391b6cf20e4bcf27b38d3d
Andy Flynn <andy.flynn@boughtbymany.com>
2020-04-28 13:49:54 +0100

    feat: adding value substitution mixins from vue-naturallang

--- src/widgets/helpers/Label.vue
+++ src/widgets/helpers/Label.vue
@@ -7,6 +7,7 @@
 <script>
 import Formatters from '../../lib/formaters'
 import { i18n as _ } from '../../lib/i18n'
+import { ValueSubstituteMixin } from '@boughtbymany/mutt-forms-vue-naturallang'
 
 export default {
   name: 'label-widget',
@@ -14,6 +15,7 @@ export default {
     'field',
     'fieldId',
   ],
+  mixins: [ValueSubstituteMixin],
   computed: {
     showLabel() {
       if (this.field.options.hasOwnProperty('showLabel')) {
@@ -40,66 +42,6 @@ export default {
       label = this.substituteValues(label)
 
       return label
-    },
-  },
-  methods: {
-    /**
-     * These methods are stolen directly from Vue-Naturallang
-     * TODO: make this use the same methods to cut down on duplication
-     */
-    /*
-     * Walk upwards until we can resolve a field name.
-     */
-    getFieldRecursively(field, fieldName) {
-      if (field.getFieldByPath) {
-        const result = field.getFieldByPath(fieldName)
-        if (result) {
-          return result
-        }
-      }
-
-      if (field.parent) {
-        return this.getFieldRecursively(field.parent, fieldName)
-      }
-
-      return null
-    },
-    /*
-     * Replace variable markers with the values from fields elsewhere in the
-     * form.
-     *
-     * This will replace the substring `$[foo]` with the value from the
-     * nearest field with that name in the form hierarchy.
-     */
-    substituteValues(string) {
-      const regex = /\$\[([^\]]+)\]/g
-      const replacements = {}
-
-      let result
-      while ((result = regex.exec(string)) !== null) {
-        let fieldName = result[1]
-        let defaultText;
-        if (fieldName.indexOf('|')) {
-          let [_field, _defaultText] = fieldName.split('|')
-          fieldName = _field.trim()
-          defaultText = _defaultText.trim()
-        }
-
-        const field = this.getFieldRecursively(this.field, fieldName)
-        if (!field) {
-          console.error(`Attempted to substitute
-                    from a non-existent field: ${fieldName}`)
-          continue
-        }
-        replacements[result[0]] = field.value || defaultText
-      }
-
-      let resultString = string
-      for (const [key, value] of Object.entries(replacements)) {
-        resultString = resultString.replace(key, value)
-      }
-
-      return resultString
     },
   },
 }
